d107df1007f872d6b872fd8c109be25a2e893bac
Kino <admin@Gabrielas-MacBook-Air.local>
2014-01-22 11:39:52 -0500

    more cleanup

--- uexplorer/uexplorer.js
+++ uexplorer/uexplorer.js
@@ -1,15 +1,4 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to uexplorer.";
-  };
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
 
   Template.streetview.rendered = function (){
     if(!this._rendered) {
