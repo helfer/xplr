67f33206d34cc50166e11bc0674c28bd49348c4a
user7 <helfer@mit.edu>
2014-01-22 11:36:11 -0500

    initial cleanup

--- uexplorer/uexplorer.js
+++ uexplorer/uexplorer.js
@@ -10,8 +10,34 @@ if (Meteor.isClient) {
         console.log("You pressed the button");
     }
   });
-}
 
+  Template.streetview.rendered = function (){
+    if(!this._rendered) {
+        this._rendered = true;
+
+        var lat = -33.867141;
+        var lng = 151.207114;
+        var point = new google.maps.LatLng(lat,lng);
+        var panoramaOptions = {
+            position:point,
+            disableDefaultUI:true 
+        };
+        var myDOMobj = document.getElementById("streetview");
+        pano = new google.maps.StreetViewPanorama(myDOMobj, panoramaOptions);
+
+    }
+  }
+
+  Template.map.rendered = function (){
+        var mapOptions = {
+          center: new google.maps.LatLng(-34.397, 150.644),
+          zoom: 8
+        };
+        var myDOMobj = document.getElementById("map-canvas");
+        var map = new google.maps.Map(myDOMobj,mapOptions);
+  }
+
+}
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
