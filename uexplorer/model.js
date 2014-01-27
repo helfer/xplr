64a016e9ce74273c3003df1da4840952847ed569
user7 <helfer@mit.edu>
2014-01-27 00:09:20 -0500

    places meh

--- uexplorer/model.js
+++ uexplorer/model.js
@@ -2,3 +2,24 @@ Guesses = new Meteor.Collection("guesses"); //user guess locations
 Locations = new Meteor.Collection("locations"); //deprecated; populated from db
 Places = new Meteor.Collection("places"); //populated from google places API
 Visits = new Meteor.Collection("visits"); //user 'visits' to places
+
+
+
+categories = ['resto','cafe','shop','arts'];
+google_queries = {
+    'resto':['Restaurant',['restaurant']],
+    'cafe':['Coffee Tea',['cafe','bakery']],
+    'shop':['Shopping',[
+        'department_store',
+        'grocery_or_supermarket',
+        'book_store',
+        'convenience_store',
+        'clothing_store',
+        'electronics_store']],
+    'arts':['Entertainment',[
+        'aquarium',
+        'casino',
+        'museum',
+        'movie_theater',
+        'stadium',
+        'zoo']]};
